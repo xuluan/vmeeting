785e8e665d5190a8bdff6dced9e30b374550497f
xuluan <rolle.xu@gmail.com>
2012-11-30 16:50:41 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vmeeting::Application.config.secret_token = '38bc23651ad094439b29d6baf8042452276030786c80e8297ec0e456fbfb26f404922888953c599f487bd65ccd094ff0df4e0ae70db7a04e67c1b646529f9d10'
